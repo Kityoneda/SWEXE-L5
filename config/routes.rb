0a41d93b14c9f7dc5a0d4838c8e0cb3dd48622d6
Kazuki Yoneda <2111140039c@ed.fuk.kindai.ac.jp>
2023-10-26 09:40:26 +0000

    create Tweet

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  # get 'tweets/index'
+  # get 'tweets/new'
+  # get 'tweets/create'
+  # get 'tweets/destroy'
   # get 'users/index'
   # get 'users/new'
   # get 'users/create'
   # get 'users/destroy'
-  resources :users
-  root 'users#index'
+ resources :tweets
+ resources :users, only: [:index, :new, :create, :destroy]
+ root 'users#index'
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_25_070059) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_26_093822) do
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
     t.integer "tweet_id"
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_25_070059) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
